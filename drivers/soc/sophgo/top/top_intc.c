3704ef61b89389b4228090f11c2b2af1a36a6953
Chen Wang <unicorn_wang@outlook.com>
2024-10-15 11:58:36 +0800

    disable dump_stack
    Signed-off-by: Chen Wang <unicorn_wang@outlook.com>

--- drivers/soc/sophgo/top/top_intc.c
+++ drivers/soc/sophgo/top/top_intc.c
@@ -126,7 +126,7 @@ static int top_intc_domain_alloc(struct irq_domain *domain,
 
 	pr_info("----> %s hwirq %ld, irq %d, plic irq %d, total %d\n", __func__,
 		hwirq, virq, data->plic_irqs[hwirq], nr_irqs);
-	dump_stack();
+	//dump_stack();
 	return 0;
 }
 
