cdcdcdd51ff9175e9704db62a9e745b535709cf7
Icenowy Zheng <uwu@icenowy.me>
2024-10-18 15:16:43 +0800

    DON'T UPSTERAM: HACK: ttm: disallow cached mapping
    FIXME: this hack fixed the issue when booting PCIe radeon GPU card,
    it will report:
    [    2.909876] [drm:r600_ring_test] *ERROR* radeon: ring 0 test failed (scratch(0x8504)=0xCAFEDEAD)
    
    Rootcasue Analysis:
    The radeon/amdgpu driver expects pcie to snoop cpu cache, which
    means the contents of cpu cache should also be seen by pcie.
    But sg2042 does not meet this requirement. When the GPU is being
    initialized, the GPU's ring_test tries to write a value to a
    certain memory address, and then read back to check whether it
    is written in (before that, the cpu writes another value in).
    Due to SG2042's issue, gpu cannot snoop cpu cache, check fails.
    
    The hack solution is to change the memory mapped by the ttm
    memory manager to uncached mode to workaround this SoC issue.
    
    Signed-off-by: Icenowy Zheng <uwu@icenowy.me>
    Tested-by: Chen Wang <unicorn_wang@outlook.com>

--- drivers/gpu/drm/ttm/ttm_tt.c
+++ drivers/gpu/drm/ttm/ttm_tt.c
@@ -159,7 +159,7 @@ static void ttm_tt_init_fields(struct ttm_tt *ttm,
 	ttm->dma_address = NULL;
 	ttm->swap_storage = NULL;
 	ttm->sg = bo->sg;
-	ttm->caching = caching;
+	ttm->caching = ttm_write_combined;
 	ttm->restore = NULL;
 	ttm->backup = NULL;
 }
